496a8efb98e5c39872aca5b8aa115e23c6be0025
Rohit <rohitkr412@gmail.com>
2018-12-18 00:28:53 +0800

    Need to figure out pagination

--- scrape.js
+++ scrape.js
@@ -1,3 +1,6 @@
+
+/*
+
 const request = require('request');
 const cheerio = require('cheerio');
 var promptList = require('prompt-list');
@@ -72,10 +75,13 @@ function SecondPage(link)
             const $ = cheerio.load(html2);
             const list = $('#div_Search_Results_Found');
             var SubCategories = [];
+            var SubCategories_links = [];
 
             $('#div_Search_Results_Found a').each((i,li2) => {
-                    const item2 =$(li2).text().replace(/\s\s+/g,'');                            
+                    const item2 =$(li2).text().replace(/\s\s+/g,''); 
+                    const link2 =$(li2).attr('href');                       
                     SubCategories.push(item2);
+                    SubCategories_links.push(link2);
             });                      
 
 
@@ -91,12 +97,66 @@ function SecondPage(link)
 
 
             PromptList2.run().then(function(answer2){
-                //console.log("The chosen category is : "+answer2); 
+                for(var i=0;i<SubCategories.length;i++)
+                {
+                    if(answer2 == SubCategories[i]) break;
+                }
+                var Sub_Category_link = SubCategories_links[i];
+                console.log("The chosen category is : "+answer2);
+                console.log("The link for the chosen category is : "+Sub_Category_link);                
             });
         }
     });
 }
 
     
+
+
+
+
+*/
+
+const request = require('request');
+const cheerio = require('cheerio');
+var promptList = require('prompt-list');
+
+
+
+
+    request('http://www.thegreenbook.com/products/analysers/page/'+2,(error,response,html) => { 
+    if(!error && response.statusCode == 200)
+    {          
+        const $ = cheerio.load(html);
+        const list4 = $('.companyInformation');
+        var Shops_Phone = [];
+        var Shops_Name = [];
+
+        $('.phoneBtn').each((j,li) => {
+            const phone =$(li).text().replace(/\s\s+/g,'').substring(8);
+            Shops_Phone.push(phone);
+        });
+
+        $('.H4Ver2').each((j,li2) => {
+            const name =$(li2).text().replace(/\s\s+/g,'');
+            Shops_Name.push(name);
+        });
+
+        if(Shops_Phone.length > 0)
+        {
+            for(var k=0;k<Shops_Phone.length;k++)
+            {
+                console.log(Shops_Name[k]+' - '+Shops_Phone[k]);
+            }
+        }
+        else
+        {
+            console.log("Shops empty - "+j);
+        }
+    }
+    });
+
+    
+
+
 
 

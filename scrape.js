1e68b79ef40108521239cf9e0216c1ad4e0668af
Rohit <rohitkr412@gmail.com>
2018-12-19 01:42:51 +0800

    Commit after recording video

--- scrape.js
+++ scrape.js
@@ -1,10 +1,8 @@
-
-/*
-
 const request = require('request');
 const cheerio = require('cheerio');
 var promptList = require('prompt-list');
-
+var fs = require('fs');
+var csv = require('fast-csv');
 
 request('http://www.thegreenbook.com/',(error,response,html) => {
     if(!error && response.statusCode == 200)
@@ -41,18 +39,7 @@ request('http://www.thegreenbook.com/',(error,response,html) => {
                 name:'order',
                 message:'Please select one:',
                 choices:categories
-        });
-
-        // PromptList.ask(function(answer){
-        //     for(var i=0;i<categories.length;i++)
-        //     {
-        //         if(answer == categories[i]) break;
-        //     }
-        //     var link = category_links[i];
-        //     console.log("The chosen category is : "+answer);
-        //     console.log("The link for the chosen category is : "+link);    
-                 
-        // });
+        });        
 
         PromptList.run().then(function(answer){
             for(var i=0;i<categories.length;i++)
@@ -60,12 +47,11 @@ request('http://www.thegreenbook.com/',(error,response,html) => {
                 if(answer == categories[i]) break;
             }
             var link = category_links[i];
-            //console.log("The chosen category is : "+answer);
-            //console.log("The link for the chosen category is : "+link);
             SecondPage(link);
         });
     }
 });
+
 
 function SecondPage(link)
 {
@@ -91,10 +77,6 @@ function SecondPage(link)
                                 choices:SubCategories
             });
 
-            // PromptList2.ask(function(answer2){
-            //         console.log("The chosen category is : "+answer2);                  
-            // });
-
 
             PromptList2.run().then(function(answer2){
                 for(var i=0;i<SubCategories.length;i++)
@@ -102,61 +84,56 @@ function SecondPage(link)
                     if(answer2 == SubCategories[i]) break;
                 }
                 var Sub_Category_link = SubCategories_links[i];
-                console.log("The chosen category is : "+answer2);
-                console.log("The link for the chosen category is : "+Sub_Category_link);                
+                Sub_Category_link = "http://www.thegreenbook.com" + Sub_Category_link;
+                ThirdPage(Sub_Category_link);             
             });
         }
     });
 }
 
+
+function ThirdPage(Sub_Category_link)
+{    
+    request(Sub_Category_link,(error,response,html) => { 
+        if(!error && response.statusCode == 200)
+        {          
+            const $ = cheerio.load(html);
+            const list4 = $('.companyInformation');
+            var Shops_Phone = [];
+            var Shops_Name = [];
+            
     
-
-
-
-
-*/
-
-const request = require('request');
-const cheerio = require('cheerio');
-var promptList = require('prompt-list');
-
-
-
-
-    request('http://www.thegreenbook.com/products/analysers/page/'+2,(error,response,html) => { 
-    if(!error && response.statusCode == 200)
-    {          
-        const $ = cheerio.load(html);
-        const list4 = $('.companyInformation');
-        var Shops_Phone = [];
-        var Shops_Name = [];
-
-        $('.phoneBtn').each((j,li) => {
-            const phone =$(li).text().replace(/\s\s+/g,'').substring(8);
-            Shops_Phone.push(phone);
-        });
-
-        $('.H4Ver2').each((j,li2) => {
-            const name =$(li2).text().replace(/\s\s+/g,'');
-            Shops_Name.push(name);
-        });
-
-        if(Shops_Phone.length > 0)
-        {
-            for(var k=0;k<Shops_Phone.length;k++)
+            $('.phoneBtn').each((j,li) => {
+                const phone =$(li).text().replace(/\s\s+/g,'').substring(8);
+                Shops_Phone.push(phone);
+            });
+    
+            $('.H4Ver2').each((j,li2) => {
+                const name =$(li2).text().replace(/\s\s+/g,'');
+                Shops_Name.push(name);
+            });
+    
+            if(Shops_Phone.length > 0)
             {
-                console.log(Shops_Name[k]+' - '+Shops_Phone[k]);
+                for(var k=0;k<Shops_Phone.length;k++)
+                {
+                    console.log(Shops_Name[k]+' - '+Shops_Phone[k]);
+                }
             }
-        }
-        else
-        {
-            console.log("Shops empty - "+j);
-        }
-    }
-    });
-
+            else
+            {
+                console.log("Shops empty - "+j);
+            }     
+            
+            var ws = fs.createWriteStream('crawl.csv');
+            const path = './crawl.csv';
     
-
-
-
+            if(!fs.existsSync(path))
+            csv.write([Shops_Name,Shops_Phone],{headers:true}).pipe(ws);
+    
+            else
+            csv.writeToStream(fs.createWriteStream(path,{flags:'a'}),[Shops_Name,Shops_Phone],{headers:true});
+        }
+        });        
+}
 
